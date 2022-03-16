69f915e56fb312bf226d1581e5d205c82b7a4db9
gatsuBerserk <berserkdev@outlook.it>
2022-03-16 15:48:03 +0100

    vue-js connection

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@
         Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
     *Bonus:
         Aggiungere alla pagina un’immagine, presa anch’essa da un data, magari volendo sperimentare anche prendendola da un input.
- */
+ */ 
+
+const app = new Vue (
+    {
+        el : "#app"
+    }
+)
